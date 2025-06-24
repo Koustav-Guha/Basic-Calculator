0bbe69cdb59874287ccf51291e94c144a86e45f6
Koustav Guha <kguha730@gmail.com>
2025-06-24 08:40:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.querySelector(".input");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+        try {
+            display.value = eval(display.value);
+        } catch(error){
+            display.value = "Error!";
+        }
+    
+}
